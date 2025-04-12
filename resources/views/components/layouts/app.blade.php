c89f65a0e6aff650b776dab7a2f2270846e5c954
Tapas Datta <tapash.me@gmail.com>
2025-04-12 19:03:34 +0600

    Sidebar and dashboard work in-progress

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,24 +1,22 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>{{ $title ?? 'Page Title' }}</title>
-      @livewireStyles
-      @vite(['resources/css/app.css', 'resources/js/app.js'])
-   </head>
-   <body class="text-gray-950 antialiased">
-      <main class="overflow-hidden bg-gray-50">
-         <div class="relative mx-auto max-w-7xl">
-           <div class="absolute -top-44 -right-60 h-60 w-[36rem] transform-gpu md:right-0 bg-linear-115 from-[#fff1be] from-28% via-[#ee87cb] via-70% to-[#b060ff] rotate-[-10deg] rounded-full blur-3xl"/>
-           </div>
-         </div>
-         <div class="isolate flex min-h-dvh items-center justify-center p-6 lg:p-8">
-            <div class="w-full max-w-md rounded-xl bg-white shadow-md ring-1 ring-black/5">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ $title ?? 'Page Title' }}</title>
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        @livewireStyles
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    </head>
+    <body>
+        <div class="relative isolate flex min-h-svh w-full bg-white max-lg:flex-col lg:bg-zinc-100 dark:bg-zinc-900 dark:lg:bg-zinc-950">
+            @include('components.sidebar')
+
+            <main class="flex flex-1 flex-col pb-2 lg:min-w-0 lg:pt-2 lg:pr-2 lg:pl-64">
                 {{ $slot }}
-            </div>
-         </div>
-      </main>
-      @livewireScripts
-   </body>
+            </main>
+        </div>
+    </body>
 </html>
--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -4,6 +4,7 @@
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>{{ $title ?? 'Page Title' }}</title>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       @livewireStyles
       @vite(['resources/css/app.css', 'resources/js/app.js'])
    </head>
