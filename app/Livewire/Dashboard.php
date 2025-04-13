da59105ffba5b2f28874fdd77097cf0a09216b69
Tapas Datta <tapash.me@gmail.com>
2025-04-13 14:44:11 +0600

    Sidebar updated

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -3,9 +3,18 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Component
 {
+    public $user;
+
+    public function mount()
+    {
+        $this->user = Auth::user();
+    }
+
     public function render()
     {
         return view('livewire.dashboard');
